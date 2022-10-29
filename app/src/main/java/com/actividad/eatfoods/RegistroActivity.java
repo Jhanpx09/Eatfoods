ca8d9b2c8b6f35add920134c997cc052f2a282bd
Jhanpx09 <116242125+Jhanpx09@users.noreply.github.com>
2022-10-29 18:44:02 -0500

    Implemento del registro

--- app/src/main/java/com/actividad/eatfoods/RegistroActivity.java
+++ app/src/main/java/com/actividad/eatfoods/RegistroActivity.java
@@ -1,37 +1,31 @@
 package com.actividad.eatfoods;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class RegistroActivity extends AppCompatActivity {
 
-    TextView register;
+
+    TextView reinicio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_registro);
         getSupportActionBar().hide();
 
 
-        register = (TextView) findViewById(R.id.registro);
-        register.setOnClickListener(new View.OnClickListener() {
+        reinicio = (TextView) findViewById(R.id.reinicio);
+        reinicio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, RegistroActivity.class));
+                startActivity(new Intent(RegistroActivity.this, MainActivity.class));
             }
         });
 
     }
-
-
-
 }
